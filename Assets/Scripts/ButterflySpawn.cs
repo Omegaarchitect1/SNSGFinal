16504f8d6ddcd82bc4de24bd177334b0a46d5241
Mitchell Quigg <Mitchell.Quigg@iam.local>
2018-12-07 14:41:23 -0600

    Created paths for other butterflies

---
+++ Assets/Scripts/ButterflySpawn.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButterflySpawn : MonoBehaviour {
+
+    private float nextspawntime;
+
+    [SerializeField]
+    private GameObject butteflyprefab;
+
+    [SerializeField]
+    private float spawnDelay;
+
+	// Update is called once per frame
+	void Update () {
+        if (ShouldSpawn())
+        {
+            Spawn();
+        }
+    }
+
+    private void Spawn()
+    {
+        nextspawntime = Time.time + spawnDelay;
+        Instantiate(butteflyprefab, transform.position, transform.rotation);
+    }
+
+    private bool ShouldSpawn()
+    {
+        return Time.time > nextspawntime;
+    }
+	
+	
+}
